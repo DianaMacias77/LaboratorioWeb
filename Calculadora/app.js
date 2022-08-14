4ce0861e73b9c926f0381d8b38799b49cc93140e
DianaMacias77 <42615630+DianaMacias77@users.noreply.github.com>
2022-08-14 16:54:22 -0500

    actividad calculadora

--- Calculadora/app.js
+++ Calculadora/app.js
@@ -1,4 +1,5 @@
 const calculadora = require('./mate')
+const interface = require('readline-sync')
 
 //Al ejecutralo mostrar un menú de opciones tipo switch
 do{
@@ -7,11 +8,38 @@ do{
     console.log('Tipos de operaciones');
     console.log('1. Suma');
     console.log('2. Resta');
-    console.log('3. Multiplicación');
-    console.log('4. División');
+    console.log('3. Multiplicacion');
+    console.log('4. Division');
     console.log('5. Salir');
-    res = console.log('Escribe la opción a realizar: ');
-}while(res !='5')
+    res = interface.question("Que operacion deseas realizar?, escribe el numero: ");
 
-let suma = calculadora.suma(n1,n2)
-console.log(suma)
+    if(res != "5"){
+        const n1 = interface.question("Dame el primer numero: ");
+        const n2 = interface.question("Dame el segundo numero: ");
+
+        switch(res){
+            case "1":
+                let suma = parseInt(calculadora.suma(n1,n2));
+                console.log("El resultado es: "+suma);
+                console.log('****************************');
+                break;
+            case "2":
+                let resta = parseInt(calculadora.resta(n1,n2));
+                console.log("El resultado es: "+resta);
+                console.log('****************************');
+                break;
+            case "3":
+                let multi = parseInt(calculadora.multi(n1,n2));
+                console.log("El resultado es: "+multi);
+                console.log('****************************');
+                break;
+            case "4":
+                let div = parseInt(calculadora.div(n1,n2));
+                console.log("El resultado es: "+div);
+                console.log('****************************');
+                break;
+            case "5":
+                break;
+        }
+    }
+}while(res != "5");
--- Calculadora/mate.js
+++ Calculadora/mate.js
@@ -1,15 +1,25 @@
 exports.suma = function(n1,n2){
-    return n1+n2
+    return n1+n2;
 }
 
-/*
 exports.resta = function(n1,n2){
-  if(n1<n2){
-    n2-n1
-  }  
-  else{
-    n1-n2
-  }
+    if(n1<n2){
+        return n2-n1;
+    }  
+    else{
+        return n1-n2;
+    }
 }
 
-*/
+exports.multi = function(n1,n2){
+    return n1*n2;
+}
+
+exports.div = function(n1,n2){
+    if(n1<n2){
+        return n2/n1;
+    }  
+    else{
+        return n1/n2;
+    }
+}
