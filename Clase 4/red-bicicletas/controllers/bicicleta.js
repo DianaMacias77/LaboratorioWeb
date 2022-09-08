ab6ac30fb3dc42993d5ba84ccaf8fafe43ab2d3b
DianaMacias77 <42615630+DianaMacias77@users.noreply.github.com>
2022-09-07 21:01:46 -0500

    no jala allbicis

--- Clase 4/red-bicicletas/controllers/bicicleta.js
+++ Clase 4/red-bicicletas/controllers/bicicleta.js
@@ -1,5 +1,7 @@
 const Bicicleta = require('../models/bicicleta')
 
 exports.bicicleta_list = function(req, res){
+    console.log(Bicicleta.allBicis)
     res.render('bicicletas/index',{bicis: Bicicleta.allBicis})
 }
+
--- Clase 4/red-bicicletas/models/bicicleta.js
+++ Clase 4/red-bicicletas/models/bicicleta.js
@@ -1,19 +1,24 @@
 let Bicicleta =function(id,color,modelo,ubicacion){
     this.id=id
-    this.color=colorthis.modelo=modelo
+    this.color=color
+    this.modelo=modelo
     this.ubicacion=ubicacion
 }
 
 Bicicleta.prototype.toString = function(){
     return 'id: ${this.id} | color: ${this.color}'
 }
-module.exports=Bicicleta
 
-Bicicleta.alBicis=[]
+Bicicleta.allBicis=[]
 
 Bicicleta.add = function(aBici){
     Bicicleta.allBicis.push(aBici)
 }
 
-let b1 = new Bicicleta(1,'rojo','BMX',[-34.6012421,58.381654])
+let b1 = new Bicicleta(1,'rojo','BMX', [-34.6012421,58.381654])
 let b2 = new Bicicleta(2,'negra','ruta', [-33.6012424,-68.381654])
+
+Bicicleta.add(b1)
+Bicicleta.add(b2)
+
+module.exports=Bicicleta
